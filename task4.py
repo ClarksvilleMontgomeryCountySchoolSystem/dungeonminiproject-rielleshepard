d3f9b50ec4155df1a38340ee3d481b28ce7f196d
rielleshepard <rielle.shepard058@students.cmcss.net>
2026-03-04 13:00:57 -0600

    Update task4.py

---
+++ task4.py
@@ -0,0 +1,6 @@
+drawbridge_raised = True
+if drawbridge_raised:
+  outcome = "Thunder: Thunder rings out and you cross the bridge."
+else:
+  outcome = "Doom: The drawbridge wont come down, you are doomed."
+print(outcome)
