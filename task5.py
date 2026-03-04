c845367b97d23e0fc8c3da2802c74c33060b7e46
rielleshepard <rielle.shepard058@students.cmcss.net>
2026-03-04 13:02:56 -0600

    Update task5.py

---
+++ task5.py
@@ -0,0 +1,6 @@
+escaped = True
+if escaped:
+  outcome = "Legend: you escaped and are known as a legend."
+else:
+  outcome = "Doom: You cant escape, you are doomed to enternal confindment."
+print(outcome)
