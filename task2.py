a730fcff2e307e83546500fc5ad35350c6dd45b0
rielleshepard <rielle.shepard058@students.cmcss.net>
2026-03-04 11:18:15 -0600

    Update task2.py

---
+++ task2.py
@@ -0,0 +1,6 @@
+has_key = True
+if has_key:
+  outcome = "Click: The key you have works on the door, you continue through the dungeon."
+else:
+  outcome = "Doom: The door wont open, you are doomed to be trapped forever."
+print(outcome)
