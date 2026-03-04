a9c076e804021689bae4699d635606b6f10d3077
rielleshepard <rielle.shepard058@students.cmcss.net>
2026-03-04 11:15:57 -0600

    Update task1.py

---
+++ task1.py
@@ -0,0 +1,6 @@
+torch_lit = True
+if torch_lit:
+  outcome = "Flicker: You may not know where you are but at least you can see."
+else:
+  outcome = "Doom: The lights wont turn on, you are doomed to eternal darkness."
+print(outcome)
