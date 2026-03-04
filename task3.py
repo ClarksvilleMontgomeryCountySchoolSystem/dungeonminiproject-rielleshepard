51a0a85fbaa207aa021c54a57d62932bc4cfc6f3
rielleshepard <rielle.shepard058@students.cmcss.net>
2026-03-04 12:54:10 -0600

    Update task3.py

---
+++ task3.py
@@ -0,0 +1,6 @@
+guard_awake = True
+if guard_awake:
+  outcome = "Thunder: Thunder rings out and you slip by the guard."
+else:
+  outcome = "Doom: The guard caught you, you are doomed."
+print(outcome)
